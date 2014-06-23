54d3eff410026f9741ffa2feb2f42b581ece1fea
Patrick Oladimeji <github@dustygem.co.uk>
2014-06-23 10:56:02 +0100

    refactored and simplified region fold so that it works for any language that is recognised by the code mirror mode

--- foldhelpers/region-fold.js
+++ foldhelpers/region-fold.js
@@ -1,56 +1,45 @@
 // Function copied from brace-fold.js addon in CodeMirror Library with minor altering.
 // CodeMirror 4.1.1, copyright (c) by Marijn Haverbeke and others
 // Distributed under an MIT license: http://codemirror.net/LICENSE
+/*jshint eqnull:true, unused:true, undef: true*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, eqeq:true */
 /*global define, brackets*/
 define(function (require, exports, module) {
     "use strict";
-    var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
-
+    var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror"),
+        startRegion = /\W+region/i,
+        endRegion = /\W+endregion/i;
+    
     module.exports = function(cm,start) {
-      var line = start.line, lineText = cm.getLine(line);
-      var startCh, tokenType, startToken = "//Region", endToken = "//Endregion";
-      function findOpening(openCh) {
-        for (var at = start.ch, pass = 0;;) {
-          var found = at <= 0 ? -1 : lineText.lastIndexOf(openCh, at - 1);
-          if (found == -1) {
-            if (pass == 1) break;
-            pass = 1;
-            at = lineText.length;
-            continue;
-          }
-          if (pass == 1 && found < start.ch) break;
-          tokenType = cm.getTokenTypeAt(CodeMirror.Pos(line, found + 1));
-          return found;
+        var line = start.line;
+        var startCh = 0, stack = [], token;
+        var lastLine = cm.lastLine(), end, endCh, nextOpen, nextClose;
+        for (var i = line; i <= lastLine; ++i) {
+            var text = cm.getLine(i), pos = startCh;
+            for (var j = pos; j < text.length; ) {
+                token = cm.getTokenAt(CodeMirror.Pos(i, j));
+                if (token && token.type === "comment") {
+                    nextOpen = token.string.toLowerCase().match(startRegion) ? token.end : -1;
+                    nextClose = token.string.toLowerCase().match(endRegion) ? token.start : -1;
+                    if (nextOpen  > -1) {
+                        stack.push(nextOpen);
+                    }
+                    if (nextClose > -1) {
+                        if (stack.length === 1) {
+                            endCh = nextClose;
+                            end = i;
+                             return {from: CodeMirror.Pos(line, stack[0]),
+                                     to: CodeMirror.Pos(end, endCh)};
+                        }
+                        stack.pop();
+                    }
+                }
+                j = token ? token.end + 1: text.length;
+            }
+            if (stack.length === 0) { break; }
         }
-      }
-
-      startCh = findOpening("//Region");
-      if (startCh == null) {
-          startToken = "<!--Region";
-          endToken = "<!--Endregion";
-          startCh = findOpening("<!--Region");
-      }
-      if (startCh == null) return;
-      startCh += (lineText.length+2);
-      var count = 1, lastLine = cm.lastLine(), end, endCh;
-      outer: for (var i = line; i <= lastLine; ++i) {
-        var text = cm.getLine(i), pos = i == line ? startCh : 0;
-        for (;;) {
-          var nextOpen = text.indexOf(startToken, pos), nextClose = text.indexOf(endToken, pos);
-          if (nextOpen < 0) nextOpen = text.length;
-          if (nextClose < 0) nextClose = text.length;
-          pos = Math.min(nextOpen, nextClose);
-          if (pos == text.length) break;
-          if (cm.getTokenTypeAt(CodeMirror.Pos(i, pos + 1)) == tokenType) {
-            if (pos == nextOpen) ++count;
-            else if (!--count) { end = i; endCh = pos; break outer; }
-          }
-          ++pos;
-        }
-      }
-      if (end == null || line == end && endCh == startCh) return;
-      return {from: CodeMirror.Pos(line, startCh),
+        if (end == null || line == end && endCh == startCh) return;
+        return {from: CodeMirror.Pos(line, stack[0]),
               to: CodeMirror.Pos(end, endCh)};
     };
 
