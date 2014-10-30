5887401cc095cd9bdfb4f63694b7cdb161622405
valtlait <valtlait@users.noreply.github.com>
2014-10-30 21:07:43 +0200

    Tidy up

--- nls/ru/strings.js
+++ nls/ru/strings.js
@@ -21,35 +21,37 @@
  * 
  */
 
-// Russian translation
+// Russian Translation
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global define */
 
 define({
-	"CODE_FOLDING_SETTINGS": "Настройки Code Folding",
-    "COLLAPSE_ALL"       :   "Свернуть все",
-    "EXPAND_ALL"         :   "Развернуть все",
-    "COLLAPSE_CURRENT"   :   "Свернуть текущий",
-    "EXPAND_CURRENT"     :   "Развернуть текущий",
-    "COLLAPSE_CUSTOM_REGIONS"	: "Свернуть текущий region",
-	//form variables region
-	"MIN_FOLD_SIZE"      : "Минимальный размер сворачивания",
-	"MIN_FOLD_SIZE_HELP" : "Минимальное число строк для сворачивания",
-	"ENABLE_REGION_FOLDING": "Включить сворачивание для region`ов",
-	"SAVE_FOLD_STATES" : "Сохранять состояние",
-	"SAVE_FOLD_STATES_HELP" : "Сохранить состояние свёрнутых/развёрнутых блоков на диск при закрытии редактора и восстановить при повторном открытии",
-	"ALWAYS_USE_INDENT_FOLD": "Всегда использовать вложеность",
-	"ALWAYS_USE_INDENT_FOLD_HELP": "Всегда использовать уровень вложенности в качестве ориентира",
-	"FADE_FOLD_BUTTONS": "Скрывать кнопки для фолдинга",
-	"FADE_FOLD_BUTTONS_HELP": "Отображать кнопки для фолдинга только при наведении",
-//	"USE_KEYBOARD_SHORTCUTS": "Использовать сочетания клавиш",
-//	"REMAP_KEYBOARD_SHORTCUTS": "Переназначить сочетания клавиш",
-	"BUTTON_SAVE": "Сохранить",
-	"BUTTON_DEFAULTS": "Востановить значения по умолчанию",
-	"BUTTON_CANCEL": "Отмена",
-	//endregion
-	"CONFIRM_RELOAD_BRACKETS": "Вы хотите перезагрузить Brackets, чтобы применить новые параметры? Вам будет предложено сохранить все изменения.",
-	"RELOAD_BRACKETS": "Перегрузить Brackets",
-	"BUTTON_RELOAD": "Перегрузить"
+    "CODE_FOLDING_SETTINGS"         : "Настройки Code Folding",
+    "COLLAPSE_ALL"                  : "Свернуть все",
+    "EXPAND_ALL"                    : "Развернуть все",
+    "COLLAPSE_CURRENT"              : "Свернуть текущий",
+    "EXPAND_CURRENT"                : "Развернуть текущий",
+    "COLLAPSE_CUSTOM_REGIONS"       : "Свернуть текущий region",
+
+    // Form variables region
+    "MIN_FOLD_SIZE"                 : "Минимальный размер сворачивания",
+    "MIN_FOLD_SIZE_HELP"            : "Минимальное число строк для сворачивания",
+    "ENABLE_REGION_FOLDING"         : "Включить сворачивание для region`ов",
+    "SAVE_FOLD_STATES"              : "Сохранять состояние",
+    "SAVE_FOLD_STATES_HELP"         : "Сохранить состояние свёрнутых/развёрнутых блоков на диск при закрытии редактора и восстановить при повторном открытии",
+    "ALWAYS_USE_INDENT_FOLD"        : "Всегда использовать вложеность",
+    "ALWAYS_USE_INDENT_FOLD_HELP"   : "Всегда использовать уровень вложенности в качестве ориентира",
+    "FADE_FOLD_BUTTONS"             : "Скрывать кнопки для фолдинга",
+    "FADE_FOLD_BUTTONS_HELP"        : "Отображать кнопки для фолдинга только при наведении",
+    //"USE_KEYBOARD_SHORTCUTS"      : "Использовать сочетания клавиш",
+    //"REMAP_KEYBOARD_SHORTCUTS"    : "Переназначить сочетания клавиш",
+    "BUTTON_SAVE"                   : "Сохранить",
+    "BUTTON_DEFAULTS"               : "Востановить значения по умолчанию",
+    "BUTTON_CANCEL"                 : "Отмена",
+
+    // Endregion
+    "CONFIRM_RELOAD_BRACKETS"       : "Вы хотите перезагрузить Brackets, чтобы применить новые параметры? Вам будет предложено сохранить все изменения.",
+    "RELOAD_BRACKETS"               : "Перегрузить Brackets",
+    "BUTTON_RELOAD"                 : "Перегрузить"
 });
