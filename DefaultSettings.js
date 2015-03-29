077d7f074eae96302653ff3f449c151cf4d8c78b
Patrick Oladimeji <patrick@dustygem.co.uk>
2015-03-29 02:35:46 +0100

    Addresses issues #128 and #129. Line folds cache is updated correctly when document changes

--- DefaultSettings.js
+++ DefaultSettings.js
@@ -4,16 +4,16 @@
  * @date 8/23/14 15:45:35 PM
  */
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
-/*global define, require, brackets, window */
+/*global define*/
 define(function (require, exports, module) {
-	"use strict";
-    
-	module.exports = {
-		minFoldSize: 2,
-		saveFoldStates: true,
-		alwaysUseIndentFold: true,
-		enableRegionFolding: true,
-		fadeFoldButtons: false,
+    "use strict";
+
+    module.exports = {
+        minFoldSize: 2,
+        saveFoldStates: true,
+        alwaysUseIndentFold: true,
+        enableRegionFolding: true,
+        fadeFoldButtons: false,
         maxFoldLevel: 2 // this value is only used when fold all is called
-	};
+    };
 });
