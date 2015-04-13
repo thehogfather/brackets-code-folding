a657d11da053872112cc3a84c11afca67cffc96f
Nazım Can <canova@Canova-MacBook-Pro.local>
2015-04-14 00:58:22 +0300

    Added Turkish Language Support
    Added Turkish Language Support

---
+++ nls/tr/strings.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+// Turkish Translation
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "ENABLE_CODE_FOLDING"           : "Code Folding'i Etkinleştir",
+    "CODE_FOLDING_SETTINGS"         : "Code Folding Ayarları",
+    "COLLAPSE_ALL"                  : "Tümünü Daralt",
+    "EXPAND_ALL"                    : "Tümünü Genişlet",
+    "COLLAPSE_CURRENT"              : "Daralt",
+    "EXPAND_CURRENT"                : "Genişlet",
+    "COLLAPSE_CUSTOM_REGIONS"       : "Özel Bölgeleri Daralt",
+
+    // Form variables region
+    "MIN_FOLD_SIZE"                 : "En az katlama boyutu",
+    "MIN_FOLD_SIZE_HELP"            : "Bir katlama için gereken en az satır sayısı",
+    "ENABLE_REGION_FOLDING"         : "Özel bölgeleri katlamayı etkinleştir",
+    "SAVE_FOLD_STATES"              : "Katlama durumlarını kaydet",
+    "SAVE_FOLD_STATES_HELP"         : "Editör kapandığında katlama durumlarını diske kaydet ve yeniden açıldığında katlamaları geri yükle",
+    "ALWAYS_USE_INDENT_FOLD"        : "Daima girinti katlamasını kullan",
+    "ALWAYS_USE_INDENT_FOLD_HELP"   : "Her zaman girinti seviyelerini katlama klavuzu olarak kullan",
+    "FADE_FOLD_BUTTONS"             : "Katlama düğmelerini gizle",
+    "FADE_FOLD_BUTTONS_HELP"        : "Fare editör kenarında olmadığı sürece katlama düğmelerini gizle",
+    "MAX_FOLD_LEVEL"                : "En fazla iç içe katlama sayısı",
+    "MAX_FOLD_LEVEL_HELP"           : "Göster -> Tümünü Daralt seçeneğine basıldığında bulunacak ve daraltılacak katlamaların sayısını sınırlamak için kullanılır. Büyük dosyalar için performansı arttırır.",
+    "BUTTON_SAVE"                   : "Kaydet",
+    "BUTTON_DEFAULTS"               : "Varsayılanları geri yükle",
+    "BUTTON_CANCEL"                 : "İptal",
+
+    // Endregion
+    "CONFIRM_RELOAD_BRACKETS"       : "Yeni ayarları uygulamak için Brackets'ı yenilemek ister misiniz? Kaydedilmemiş belgelerdeki değişiklikleri kaydetmeniz için size sorulacaktır.",
+    "RELOAD_BRACKETS"               : "Brackets'ı Yenile",
+    "BUTTON_RELOAD"                 : "Yenile"
+});
